53bdf5e62db280fcbda969281ce6cdfa7d6e4d54
maliahgp <maliahgp@ad.unc.edu>
2024-04-15 19:04:27 -0400

    mars div setup

---
+++ rig.js
@@ -0,0 +1,7 @@
+let goodbye_mars_div = document.createElement('div');
+
+goodbye_mars_div.innerHTML = "<h2>Goodbye, Mars!</h2>";
+
+let rig_div = document.getElementById('rig');
+
+rig_div.append(goodbye_mars_div)
