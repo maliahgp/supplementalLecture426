36eac4ca77d41a23b7d9799dad97d6f51c80d682
maliahgp <maliahgp@ad.unc.edu>
2024-04-15 19:20:06 -0400

    rig function setup

--- rig.js
+++ rig.js
@@ -1,7 +1,11 @@
-let goodbye_mars_div = document.createElement('div');
-
-goodbye_mars_div.innerHTML = "<h2>Goodbye, Mars!</h2>";
-
+let make_message_div = function (message) {
+    let message_div = document.createElement('div');
+    let message_h2 = document.createElement('h2');
+    message_h2.append(document.createTextNode(message));
+    message_div.append(message_h2);
+    return message_div;
+};
 let rig_div = document.getElementById('rig');
 
-rig_div.append(goodbye_mars_div)
+rig_div.append(make_message_div("Bye, Bye< Mars!"));
+
